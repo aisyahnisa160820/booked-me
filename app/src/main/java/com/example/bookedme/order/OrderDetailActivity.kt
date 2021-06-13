14267f4ba27d5474895828b111dcbdd9e25f9f95
Ni Nyoman Wahyuni Indraswari <nunikyuni0300@gmail.com>
2021-06-13 19:44:06 +0800

    Push Activity Splash, boarding, cart, order

---
+++ app/src/main/java/com/example/bookedme/order/OrderDetailActivity.kt
@@ -0,0 +1,12 @@
+package com.example.cartbookedme.order
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.cartbookedme.R
+
+class OrderDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_order_detail)
+    }
+}
